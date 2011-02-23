814d87398c073696e5cc3398333719d313d77eb9
Mircea Ştefan Ghideu <mirhciulica@archlinux.us>
2011-02-23 13:55:29 +0200

    added uzbl configs

---
+++ configs/uzbl/scripts/undo_exit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+UNDO="$HOME/temp/uzbl_undolist"
+if [ -e $UNDO ]; then
+    URL=`tail -n 1 $UNDO`
+    LINECOUNT=`cat $UNDO | wc -l`
+    if [[ $LINECOUNT == 1 ]]; then
+        rm $UNDO
+    else
+        sed -i '$d' $UNDO
+    fi
+  echo "event NEW_TAB $URL" | socat - unix-connect:$UZBL_SOCKET
+fi
---
+++ configs/uzbl/scripts/undolist.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+UNDO="$HOME/temp/uzbl_undolist"
+if [ -e $UNDO ]; then
+    LINECOUNT=`cat $UNDO | wc -l`
+    if [ $LINECOUNT -ge 100 ]; then
+        sed -i "1d" $UNDO
+    fi
+fi
+echo "$UZBL_URI" >> $UNDO
